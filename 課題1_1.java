6bd6e14d8d020db6ebb840bcd49b8a15ccc9504c
daiki2828 <barce.flump0321@gmail.com>
2023-02-25 17:24:09 +0900

    PracticeP

---
+++ 課題1_1.java
@@ -0,0 +1,15 @@
+
+public class 課題1_1 {
+
+	public static void main(String[] args) {
+		
+		//合計値＝d
+		int d=0;
+		
+	    for (int i=1; i <= 100; i++) {
+	    	d += i;
+	    }
+        System.out.println(d);
+	}
+
+}
---
+++ 課題1_2.java
@@ -0,0 +1,17 @@
+
+public class 課題1_2 {
+
+	public static void main(String[] args) {
+		
+		int d =0;
+		
+		for (int i=2; i<=100; i++) {
+			if ( i%2 == 0 ) {
+				d += i;
+			}
+		}
+		System.out.println(d);
+
+	}
+
+}
