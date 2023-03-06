2bf5b1daf3f334c4a8e02e0fc27ca3843b2ba97b
daiki2828 <barce.flump0321@gmail.com>
2023-03-06 20:40:03 +0900

    add

---
+++ practice1_1.java
@@ -0,0 +1,14 @@
+
+public class practice1_1 {
+	
+	public static void main(String[] args) {
+		
+		int total=0;
+		
+		for (int number=1; number<=100; number++) {
+			total += number;
+		}
+		
+		System.out.println(total);
+	}
+}
---
+++ practice2_2.java
@@ -0,0 +1,14 @@
+
+public class practice2_2 {
+
+	public static void main(String[] args) {
+		 
+		int sum=0;
+		
+		for (int number=2; number<=100; number +=2) {
+			sum += number;
+		}
+        System.out.println(sum);
+	}
+
+}
